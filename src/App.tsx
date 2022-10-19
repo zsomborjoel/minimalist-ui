0c93ca1ef507ee55dc761cc712b433c7fe24f6f4
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2022-10-19 15:08:51 +0200

    Margin left 4

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <head>
+                <title>Minimalist ui page</title>
+            </head>
+            <main>
+                <section>
+                    <nav>
+                        <ul>
+                            <li>a</li>
+                        </ul>
+                    </nav>
+                </section>
+                <section className="absolute bottom-0 h-60 w-auto">
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+                <section>
+                    <h1>Nice Curves</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur earum esse pariatur
+                        tenetur atque nostrum rem necessitatibus tempore consequatur dolor quisquam, consequuntur, quae,
+                        quos optio reprehenderit excepturi! Facere, nemo.
+                    </p>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
